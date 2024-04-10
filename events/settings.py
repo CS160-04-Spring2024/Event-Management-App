07b3ba951ec5a2748fc9ca43c904bffe65198980
pranv11 <pranavpandey2720@gmail.com>
2024-04-09 20:08:53 -0700

    structural change and models included

--- events/settings.py
+++ events/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "application",
+    "debug_toolbar"
 ]
 
 MIDDLEWARE = [
@@ -47,9 +49,16 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",  
 ]
 
 ROOT_URLCONF = "events.urls"
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 TEMPLATES = [
     {
