85b0e993c3b7089b5417ee4e7724f732057ba11b
Jared SJ <jared.soliven@sjsu.edu>
2024-04-23 00:23:34 -0700

    initial commit admin dash/club_dash

--- events/club_admin/models.py
+++ events/club_admin/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class dummyClub:
+    name: str
+
+class dummyEvent:
+    name: str
--- events/club_admin/urls.py
+++ events/club_admin/urls.py
@@ -5,6 +5,7 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    path('/event/create', views.create_event, name='createEvent'),
-
+    path('event/create', views.create_event, name='createEvent'),
+    path('dashboard/admin', views.admin_dash, name='adminDash'),
+    path('dashboard/admin/<club>', views.admin_dash_club, name='adminDashClub'),
 ]
