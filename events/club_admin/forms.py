08f1aa287328f3320d9d6e51a7af2e82ac7f72c1
pranv11 <pranavpandey2720@gmail.com>
2024-05-04 23:45:32 -0700

    admin initial

--- events/club_admin/forms.py
+++ events/club_admin/forms.py
@@ -70,3 +70,43 @@ class EventForm(forms.ModelForm):
             raise forms.ValidationError('Please select at least two tags')
 
         return cleaned_data
+
+
+class EventEditForm(forms.ModelForm):
+
+    location = forms.ModelChoiceField(queryset=Location.objects.all(), widget=forms.Select(
+        attrs={'class': 'form-control shadow-sm '}))
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple(attrs={'class': ''}))
+
+    class Meta:
+        model = Event
+        fields = ['event_name', 'description', 'mode_of_operation',
+                  'start_time', 'end_time', 'fees', 'event_image', 'location', 'tags']
+        widgets = {
+            'event_name': forms.TextInput(attrs={'class': 'form-control shadow-sm rounded-pill', 'required': True}),
+            'start_time': forms.DateTimeInput(attrs={'class': 'form-control shadow-sm rounded-pill', 'type': 'datetime-local', 'required': True}),
+            'end_time': forms.DateTimeInput(attrs={'class': 'form-control shadow-sm rounded-pill', 'type': 'datetime-local', 'required': True}),
+            'mode_of_operation': forms.Select(choices=modes, attrs={'class': 'form-control shadow-sm rounded-pill'}),
+            'fees': forms.NumberInput(attrs={'class': 'form-control shadow-sm', 'type': 'number', 'required': True, 'default': 0.00, 'min': 0.00, 'max': 1000.00}),
+            'description': forms.Textarea(attrs={'rows': 10, 'class': 'form-control shadow-sm', 'required': True, 'minlength': 3}),
+            'event_image': forms.URLInput(attrs={'class': 'form-control shadow-sm'})
+        }
+
+    def clean(self):
+        cleaned_data = super(EventEditForm, self).clean()
+
+        start_time = cleaned_data.get('start_time')
+        end_time = cleaned_data.get('end_time')
+        tags = cleaned_data.get('tags')
+
+        if end_time < timezone.now():
+            raise forms.ValidationError('Selected end time is invalid')
+
+        if start_time >= end_time:
+            raise forms.ValidationError('Start and end times are infeasible')
+
+        if tags is None or len(tags) < 2:
+            raise forms.ValidationError('Please select at least two tags')
+
+        return cleaned_data
